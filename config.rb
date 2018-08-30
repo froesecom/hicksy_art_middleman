8da13d970edc29700125908963fa8cb5698df3d4
Erik the Viking <erik@hhd.com.au>
2018-08-30 16:51:53 +1000

    add dato gem

--- config.rb
+++ config.rb
@@ -1,6 +1,8 @@
 # Activate and configure extensions
 # https://middlemanapp.com/advanced/configuration/#configuring-extensions
 
+activate :dato, live_reload: true
+
 activate :autoprefixer do |prefix|
   prefix.browsers = "last 2 versions"
 end
