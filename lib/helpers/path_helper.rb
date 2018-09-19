b73bab9cc213c1c31c87c50687cd404ce100d856
Erik the Viking <erik@hhd.com.au>
2018-09-19 10:01:18 +1000

    add painting_path helper method

--- lib/helpers/path_helper.rb
+++ lib/helpers/path_helper.rb
@@ -1,7 +1,19 @@
 module PathHelper
 
   def painting_category_path(pc)
-    "/paintings/#{pc.slug}.html"
+    "#{base_painting_category_path(pc)}.html"
+  end
+
+  def painting_path(p)
+    "#{base_painting_category_path(p.painting_category)}/#{p.slug}.html"
+  end
+
+  def paintings_path
+    "/paintings"
+  end
+
+  def base_painting_category_path(pc)
+    "#{paintings_path}/#{pc.slug}"
   end
 
 end
