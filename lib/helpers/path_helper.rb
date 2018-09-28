ad7af8c5f1c254dd79d09341f47fc18f3e41084a
Erik Froese <froesecom@gmail.com>
2018-09-28 13:41:36 +1000

    add html to the paths to try to fix issue where pages not being found in netlify

--- lib/helpers/path_helper.rb
+++ lib/helpers/path_helper.rb
@@ -1,23 +1,23 @@
 module PathHelper
 
   def page_path(page)
-    "/#{page.slug}"
+    "/#{page.slug}.html"
   end
 
   def painting_category_path(pc)
-    "#{base_painting_category_path(pc)}"
+    "#{base_painting_category_path(pc)}.html"
   end
 
   def painting_path(p)
-    "#{base_painting_category_path(p.painting_category)}/#{p.slug}"
+    "#{base_painting_category_path(p.painting_category)}/#{p.slug}.html"
   end
 
   def paintings_path
-    "/paintings"
+    "/paintings.html"
   end
 
   def base_painting_category_path(pc)
-    "#{paintings_path}/#{pc.slug}"
+    "#{paintings_path}/#{pc.slug}.html"
   end
 
 end
