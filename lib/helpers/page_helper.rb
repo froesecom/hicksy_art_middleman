32d43a229318bf2bef499300e499a53e5ab72643
Erik Froese <froesecom@gmail.com>
2018-09-29 06:37:39 +1000

    make contact page dynamic

--- lib/helpers/page_helper.rb
+++ lib/helpers/page_helper.rb
@@ -2,7 +2,8 @@ module PageHelper
 
   def page_partial(page)
     slug_to_partial_map = {
-      "paintings" => "painting_categories"
+      "paintings" => "painting_categories",
+      "contact" => "contact_form"
     }
     slug_to_partial_map[page.slug]
   end
