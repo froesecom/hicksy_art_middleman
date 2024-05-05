02a02e7b3e75bdca917e9e0ad51799f2a0b63015
Erik F <froesecom@gmail.com>
2024-05-05 18:12:13 +1000

    feat: add canonical tags

--- lib/helpers/path_helper.rb
+++ lib/helpers/path_helper.rb
@@ -1,15 +1,24 @@
 module PathHelper
+  PROD_DOMAIN = "https://allenhicks.art"
 
   def page_path(page)
     "/#{page.slug}.html"
   end
 
-  def painting_category_path(pc)
-    "#{base_painting_category_path(pc)}.html"
+  def painting_category_path(pc, absolute=false)
+    path = "#{base_painting_category_path(pc)}.html"
+    if absolute
+      return absolute_path(path)
+    end
+    path
   end
 
-  def painting_path(p)
-    "#{base_painting_category_path(p.painting_category)}/#{p.slug}.html"
+  def painting_path(p, absolute=false)
+    path = "#{base_painting_category_path(p.painting_category)}/#{p.slug}.html"
+    if absolute
+      return absolute_path(path)
+    end
+    path
   end
 
   def paintings_path
@@ -23,5 +32,8 @@ module PathHelper
   def base_painting_category_path(pc)
     "#{base_paintings_path}/#{pc.slug}"
   end
-
+  
+  def absolute_path(path)
+    "#{self.class::PROD_DOMAIN}#{path}".split(".html").first
+  end
 end
